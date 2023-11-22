0fcb6fd2c6c7233b7e29cd7d052cc974671d819d
florianb <florian.bemmann@ifi.lmu.de>
2023-11-22 09:51:12 +0100

    prepare works

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
+import React, { useEffect, useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+import * as tf from '@tensorflow/tfjs';
+import {bundleResourceIO} from "@tensorflow/tfjs-react-native";
+
+
+// Whether to load model from app bundle (true) or through network (false).
+const LOAD_MODEL_FROM_BUNDLE = false;
+
+
 
 export default function App() {
+  const [tfReady, setTfReady] = useState(false);
+  const [model, setModel] = useState(false);
+
+
+  useEffect(() => {
+    async function prepare() {
+
+        // Wait for tfjs to initialize the backend.
+       await tf.ready();
+
+       const modelJson = require('./offline_model/model.json');
+       const modelWeights1 = require('./offline_model/group1-shard1of1.bin');
+       const modelUrl = bundleResourceIO(modelJson, [
+           modelWeights1,
+       ]);
+
+      const model = await tf.loadLayersModel(modelUrl)
+      setModel(model);
+
+      // Ready!
+      console.warn("TF now ready!")
+      setTfReady(true);
+    }
+    prepare();
+  }, []);
+
+  const makePrediction = function(){
+    if(tfReady) {
+      const result = model.predict(tf.tensor2d([20], [1, 1]))
+      alert(result)
+    }
+    else {
+      console.warn("TF model not ready")
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title={"Predict the Future!"} onPress={makePrediction}/>
     </View>
   );
 }
